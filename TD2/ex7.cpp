c6e639acedd793db9f471cc8497ed3c7444d2130
abdelaziz belhachmi <58863541+abdelaziz-belhachmi@users.noreply.github.com>
2023-10-25 01:23:53 +0100

    vf

--- TD2/ex7.cpp
+++ TD2/ex7.cpp
@@ -1,3 +1,37 @@
-//
-// Created by unknow on 10/22/2023.
-//
+#include <iostream>
+using std::cout, std::cin, std::endl;
+
+int main(){
+
+    int t[10];
+
+    for (int i = 0; i < 10; ++i) {
+        cout<<"enter la valeur pour t["<<i<<"]"<<endl;
+        cin>>t[i];
+    }
+
+//    tri avec tri bulle
+int tmp;
+
+    for (int i=0 ; i < 10-1; i++)
+    {
+        for (int j=0 ; j < 10-i-1; j++)
+        {
+            /* Pour un ordre décroissant utiliser < */
+            if (t[j] > t[j+1])
+            {
+                tmp = t[j];
+                t[j] = t[j+1];
+                t[j+1] = tmp;
+            }
+        }
+    }
+
+// affiche le tableau pour voir la resultas
+    for (int i = 0; i < 10; ++i) {
+        cout<<"t["<<i<<"]="<<t[i]<<endl;
+    }
+
+
+    return 0;
+}
